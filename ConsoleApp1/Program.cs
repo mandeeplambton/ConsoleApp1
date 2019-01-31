e0ed534d70deebdcbd623edf3b3525df3801ea88
c0730387 <mdeepkaur174@gmail.com>
2019-01-31 15:13:18 -0500

    commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,11 +2,67 @@
 
 namespace ConsoleApp1
 {
-    class Program
+
+
+    class village
     {
+        public static int numberofvillages = 0;
+        public village nextvillage;
+        public village previouslyvillage;
+        public string villagename;
+        public bool isAstrildehere = false;
+
+        public village()
+        {
+            village.numberofvillages++;
+        }
+
+
+        class countryside
+        {
+            public village Maple;
+
+            public village Toronto;
+            public village Ajax;
+            public village Head;
+            public village Tail;
+            public village Temp;
+
+            public void MapInitializer()
+            {
+                Maple = new village();
+                Maple.villagename = "Maple";
+                Maple.previouslyvillage = null;
+                Maple.nextvillage = Toronto;
+                Toronto = new village();
+                Toronto.villagename = "Toronto ";
+                Toronto.previouslyvillage = Maple;
+                Toronto.nextvillage = Ajax;
+                Ajax = new village();
+                Ajax.villagename = "Ajax";
+                Ajax.previouslyvillage = Toronto;
+                Ajax.nextvillage = null;
+            }
+
+            public void LookForAstride()
+            {
+                Head = Maple;
+                if (Head.isAstrildehere)
+                {
+                    Console.WriteLine("yeah! AstrIde is in");
+                }
+                while (true)
+                { }
+
+            }
+
+
+
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+
     }
 }
